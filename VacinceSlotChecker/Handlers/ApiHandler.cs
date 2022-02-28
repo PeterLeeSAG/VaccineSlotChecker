4ad5c9150536f69e524eeb221957da69ec4c1369
Peter Lee <PeterLee@southasiagroup.com>
2022-02-28 10:31:57 +0800

    Update center list and student path for Sinovac

--- VacinceSlotChecker/Handlers/ApiHandler.cs
+++ VacinceSlotChecker/Handlers/ApiHandler.cs
@@ -12,7 +12,10 @@ namespace VaccinceSlotChecker.Handlers
 {
     public static class ApiHandler
     {
-
+        //{"cv_name": "Sinovac", "avaliable_distric_id": ["18","17","16","15","14","13","12","11","8","7","5","4","3","2","1"], "avaliable_ctc_id": ["202","201","177","149","147","146","125","124","123","122","121","70","18","16","11","9","6","4","3"]}
+        //https://booking.covidvaccine.gov.hk/forms/api_center_by_cv_name?cv_name=Sinovac
+        //https://booking.covidvaccine.gov.hk/forms/api_center_by_cv_name?cv_name=Biontech/Fosun
+               
         //Execute the schedule job on the remote api
         public static async Task<CenterSlotResult> Execute(string centerID, string cvCenterType, string cvName)
         {
--- VacinceSlotChecker/Program.cs
+++ VacinceSlotChecker/Program.cs
@@ -27,8 +27,8 @@ namespace VaccinceSlotChecker
                 var input = Console.ReadLine();
                 input = input.Trim().ToUpper();
 
-                if (input == "B")
-                {
+                //if (input == "B")
+                //{
                     while (isChildrenChioce == false)
                     {
                         Console.Write("Children Only? (Y/N)");
@@ -41,7 +41,7 @@ namespace VaccinceSlotChecker
                             isChildrenOnly = cinput == "Y" ? true : false;
                         }
                     }
-                }
+                //}
 
                 if (input == "B" || input == "S")
                 {
@@ -49,10 +49,10 @@ namespace VaccinceSlotChecker
                     var choice = input == "B" ? "BioNTech/Fosun" : (input == "S" ? "Sinovac" : "");
                     centers = centers.Where(c => c.cv_name == choice).ToList();
 
-                    if (input == "B")
-                    {
-                        centers = centers.Where(c => isChildrenOnly ? c.center_name.Contains("兒童") : !c.center_name.Contains("兒童")).ToList();
-                    }
+                    //if (input == "B")
+                    //{
+                        centers = centers.Where(c => isChildrenOnly ? c.center_name.Contains("兒童") || c.center_name.Contains("3至17歲") || c.center_name.Contains("3-17 歲") : !c.center_name.Contains("兒童") && !c.center_name.Contains("3至17歲") && !c.center_name.Contains("3-17 歲")).ToList();
+                    //}
                 }
             }
 
@@ -170,7 +170,7 @@ namespace VaccinceSlotChecker
                             foreach (var s in g)
                             {
 
-                                displaySlots += (string.IsNullOrEmpty(displaySlots) ? "" : ", ") + s.datetime.ToString("hh:mm");
+                                displaySlots += (string.IsNullOrEmpty(displaySlots) ? "" : ", ") + s.datetime.ToString("HH:mm");
                             }
 
                             Console.WriteLine("TimeSlots: " + displaySlots);
@@ -185,7 +185,7 @@ namespace VaccinceSlotChecker
                 {
                     Console.Clear();
                     Console.WriteLine("<<Data updated @ " + DateTime.Now.ToString("HH:mm:ss") + ">>");
-                    Console.WriteLine("All centers have not free time-slot!");
+                    Console.WriteLine("All centers have no free time-slot!");
                     Thread.Sleep(3000);
                 }
             }
@@ -212,32 +212,45 @@ namespace VaccinceSlotChecker
             result.Add(new VaccineCenter { center_id = "18", cv_ctc_type = "HA",  cv_name = "Sinovac", district_name = "沙田區", center_name = "馬鞍山家庭醫學中心" });
             result.Add(new VaccineCenter { center_id = "16", cv_ctc_type = "HA",  cv_name = "Sinovac", district_name = "荃灣區", center_name = "伍若瑜夫人普通科門診診所" });
             result.Add(new VaccineCenter { center_id = "22", cv_ctc_type = "HA",  cv_name = "Sinovac", district_name = "天水圍區", center_name = "天水圍(天業路)社區健康中心" });
+            result.Add(new VaccineCenter { center_id = "122", cv_ctc_type = "DH", cv_name = "Sinovac", district_name = "中西區", center_name = "西環學生健康服務中心 (3至17歲)" });
+            result.Add(new VaccineCenter { center_id = "121", cv_ctc_type = "DH", cv_name = "Sinovac", district_name = "東區", center_name = "柴灣學生健康服務中心 (3至17歲)" });
+            result.Add(new VaccineCenter { center_id = "201", cv_ctc_type = "CVC", cv_name = "Sinovac", district_name = "灣仔區", center_name = "香港中央圖書館(展覽館)社區疫苗接種中心 (只限  3-17 歲)" });
+            result.Add(new VaccineCenter { center_id = "202", cv_ctc_type = "CVC", cv_name = "Sinovac", district_name = "灣仔區", center_name = "香港中央圖書館(展覽館)社區疫苗接種中心 (只限 60歲或以上)" });
+            result.Add(new VaccineCenter { center_id = "125", cv_ctc_type = "DH", cv_name = "Sinovac", district_name = "沙田區", center_name = "沙田學生健康服務中心 (3至17歲)" });
+            result.Add(new VaccineCenter { center_id = "124", cv_ctc_type = "DH", cv_name = "Sinovac", district_name = "屯門區", center_name = "屯門學生健康服務中心 (3至17歲)" });
+            //result.Add(new VaccineCenter { center_id = "", cv_ctc_type = "", cv_name = "Sinovac", district_name = "", center_name = "" });            
 
             ////BNT
-            result.Add(new VaccineCenter { center_id = "25", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "(復必泰)中山紀念公園體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "179", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "(復必泰)西營盤賽馬會普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "28", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "西灣河體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "42", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "香港大學駐港怡醫院社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "192", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "(復必泰)香港大學駐港怡醫院兒童社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "178", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "(復必泰)貝夫人普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "189", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "(復必泰)禮頓中心衛星社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "172", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "教育局九龍塘教育服務中心社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "190", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "(復必泰)香港兒童醫院兒童社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "46", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "曉光街體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "174", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "九龍灣體育館社區疫苗接種中心(復必泰)" });
-            result.Add(new VaccineCenter { center_id = "181", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "(復必泰)觀塘社區健康中心" });
-            result.Add(new VaccineCenter { center_id = "37", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "荔枝角公園體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "187", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "(復必泰)北大嶼山社區健康中心 ( 北大嶼山醫院 )" });
-            result.Add(new VaccineCenter { center_id = "142", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "林士德體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "183", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "南葵涌賽馬會普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "185", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "粉嶺家庭醫學中心" });
-            result.Add(new VaccineCenter { center_id = "182", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "將軍澳（寶寧路）普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "36", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "香港中文大學醫院社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "191", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "", center_name = "圓洲角體育館兒童社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "184", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "大埔賽馬會普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "186", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "仁愛普通科門診診所" });
-            result.Add(new VaccineCenter { center_id = "27", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "", center_name = "元朗體育館社區疫苗接種中心" });
-            result.Add(new VaccineCenter { center_id = "188", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "", center_name = "容鳳書健康中心" });
+            result.Add(new VaccineCenter { center_id = "25", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "中西區", center_name = "(復必泰)中山紀念公園體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "179", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "中西區", center_name = "(復必泰)西營盤賽馬會普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "28", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "東區", center_name = "西灣河體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "42", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "南區", center_name = "香港大學駐港怡醫院社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "192", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "南區", center_name = "(復必泰)香港大學駐港怡醫院兒童社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "178", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "灣仔區", center_name = "(復必泰)貝夫人普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "189", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "灣仔區", center_name = "(復必泰)禮頓中心衛星社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "172", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "九龍城區", center_name = "教育局九龍塘教育服務中心社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "190", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "觀塘區", center_name = "(復必泰)香港兒童醫院兒童社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "46", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "觀塘區", center_name = "曉光街體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "174", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "觀塘區", center_name = "九龍灣體育館社區疫苗接種中心(復必泰)" });
+            result.Add(new VaccineCenter { center_id = "181", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "觀塘區", center_name = "(復必泰)觀塘社區健康中心" });
+            result.Add(new VaccineCenter { center_id = "37", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "深水埗區", center_name = "荔枝角公園體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "187", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "離島區", center_name = "(復必泰)北大嶼山社區健康中心 ( 北大嶼山醫院 )" });
+            result.Add(new VaccineCenter { center_id = "142", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "葵青區", center_name = "林士德體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "183", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "葵青區", center_name = "南葵涌賽馬會普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "185", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "北區", center_name = "粉嶺家庭醫學中心" });
+            result.Add(new VaccineCenter { center_id = "182", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "西貢區", center_name = "將軍澳（寶寧路）普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "36", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "沙田區", center_name = "香港中文大學醫院社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "191", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun",district_name = "沙田區", center_name = "(復必泰)圓洲角體育館兒童社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "184", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "大埔區", center_name = "(復必泰)仁愛普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "186", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "屯門區", center_name = "仁愛普通科門診診所" });
+            result.Add(new VaccineCenter { center_id = "27", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosun", district_name = "元朗區", center_name = "元朗體育館社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "188", cv_ctc_type = "HA", cv_name = "BioNTech/Fosun", district_name = "元朗區", center_name = "容鳳書健康中心" });
+            result.Add(new VaccineCenter { center_id = "199", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosu", district_name = "中西區", center_name = "香港大學陸佑堂社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "33", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosu", district_name = "油尖旺區", center_name = "(復必泰)界限街體育館(一號及二號)社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "198", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosu", district_name = "北區", center_name = "和興遊樂場短期社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "197", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosu", district_name = "沙田區", center_name = "沙田大會堂短期社區疫苗接種中心" });
+            result.Add(new VaccineCenter { center_id = "196", cv_ctc_type = "CVC", cv_name = "BioNTech/Fosu", district_name = "屯門區", center_name = "屯門大會堂短期社區疫苗接種中心" });            
+            //result.Add(new VaccineCenter { center_id = "", cv_ctc_type = "", cv_name = "BioNTech/Fosu", district_name = "", center_name = "" });
 
             return result;
         }
